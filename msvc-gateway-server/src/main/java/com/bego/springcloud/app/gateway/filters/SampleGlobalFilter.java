53357b4564f57bff60297b39cdc4411a331927c8
Begoña Morillas Guijarro <darknayru@gmail.com>
2025-10-21 13:33:05 +0200

    Modificar el Response con el filtro POST

---
+++ msvc-gateway-server/src/main/java/com/bego/springcloud/app/gateway/filters/SampleGlobalFilter.java
@@ -0,0 +1,33 @@
+package com.bego.springcloud.app.gateway.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class SampleGlobalFilter implements GlobalFilter {
+
+    private final Logger logger = LoggerFactory.getLogger(SampleGlobalFilter.class);
+
+    // Implementación del filtro global para el gateway
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("Ejecutando filtro antes del request PRE");
+
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            logger.info("Ejecutando filtro POST después del response");
+            // Añadiendo una cookie al response
+            exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "red").build());
+            // Modificando el tipo de contenido del response a texto plano
+            exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
+        }));
+    }
+
+}
